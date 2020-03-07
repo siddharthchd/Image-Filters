66e848badce7b26c598eff0df2478bb4dd263d5b
siddharthchd <siddharth.chaudhary1705@gmail.com>
2020-03-07 05:52:24 -0500

    filters

--- filters.py
+++ filters.py
@@ -5,14 +5,28 @@ from skimage.color import rgb2gray
 from scipy import signal
 import math
 
-# Read the image and be sure it is grayscale
-im_path = 'Image-Filters/lena.png'
-im = io.imread(im_path)
-im = rgb2gray(im)
+def boxFilter(n):
+    img_path = 'lena.png'
+    img = io.imread(img_path)
+    img = rgb2gray(img)
+    
+    img = np.pad(img, pad_width = int(n/2), mode='constant', constant_values = 0)
+    img_out = img.copy()
+    
+    height = img.shape[0]
+    width = img.shape[1]
 
-# Ensure the image is a float in the range [0-1]
-im = ((im - np.min(im)) * (1/(np.max(im) - np.min(im)) *1.0)).astype('float')
-
-# Show the image
-fig1 = plt.figure(figsize = (7,7))
-plt.imshow(im, cmap='gray')
+    for i in np.arange(0, height):
+        for j in np.arange(0, width):        
+            sum = 0
+            for k in np.arange(-(int(n/2)), int(n/2)):
+                for l in np.arange(-(int(n/2)), int(n/2)):
+                    a = img.item(i+k, j+l)
+                    sum = sum + a
+            b = int(sum / (n * n))
+            img_out.itemset((i,j), b)
+    
+    fig1 = plt.figure(figsize = (7,7))
+    plt.imshow(img, cmap='gray')
+    
+boxFilter(3)
